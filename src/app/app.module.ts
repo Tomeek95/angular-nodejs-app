7d606c1e2ce596e130c2499048a3b3c856508fed
Tomeek95 <34217893+Tomeek95@users.noreply.github.com>
2020-08-05 15:29:53 +0200

    copied existed ang app

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms"; // this is needed for using ngModul
+import { MatInputModule } from "@angular/material/input"; // path has to be specified
+import { MatCardModule } from "@angular/material/card"; // path has to be specified
+import { MatButtonModule } from "@angular/material/button"; // path has to be specified
+import { MatToolbarModule } from "@angular/material/toolbar"; // path has to be specified
+import { MatExpansionModule } from "@angular/material/expansion"; // path has to be specified
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { PostCreateComponent } from "./posts/post-create/post-create.component";
+import { HeaderComponent } from "./header/header.component";
+import { PostListComponent } from "./posts/post-list/post-list.component";
+
+@NgModule({
+    declarations: [ AppComponent, PostCreateComponent, HeaderComponent, PostListComponent ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatCardModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatExpansionModule
+    ],
+    providers: [],
+    //this array tells angular to search for Appcomponent's selector (app-route) in index.html
+    bootstrap: [ AppComponent ]
+})
+export class AppModule {}
