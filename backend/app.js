1c1487785dea6bddd5ac876b72946db72514b73e
Tomeek95 <34217893+Tomeek95@users.noreply.github.com>
2020-08-05 18:28:44 +0200

    cors error is solved

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,18 @@ const express = require("express");
 //this is a constant since we never do override the app
 const app = express();
 
+//correcting cors error -->
+//1. Header --> this means that it does not matter which domain the apps are running on, it is allowed to get resources
+//2. Header --> domains sending request with certain default headers,
+//      but that way we enable other headers
+//3. Header --> which http methods will be used
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, PUT, OPTIONS");
+    next();
+});
+
 //the first argument is path, and the others are filters as well, but the last one is a function
 //which exectues certain commands that we write in
 app.use("/api/posts", (req, res, next) => {
