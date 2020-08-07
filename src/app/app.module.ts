d2316508e315448735870984ed4f0059e60449a7
Tomeek95 <34217893+Tomeek95@users.noreply.github.com>
2020-08-07 15:59:22 +0200

    started using reactive form, insted of ngForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms"; // this is needed for using ngModul
+import { ReactiveFormsModule } from "@angular/forms"; // this is needed for using ngModul
 import { MatInputModule } from "@angular/material/input"; // path has to be specified
 import { MatCardModule } from "@angular/material/card"; // path has to be specified
 import { MatButtonModule } from "@angular/material/button"; // path has to be specified
 import { MatToolbarModule } from "@angular/material/toolbar"; // path has to be specified
 import { MatExpansionModule } from "@angular/material/expansion"; // path has to be specified
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner"; // path has to be specified
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from "./header/header.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
-import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
     declarations: [ AppComponent, PostCreateComponent, HeaderComponent, PostListComponent ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        FormsModule,
+        ReactiveFormsModule,
         BrowserAnimationsModule,
         MatInputModule,
         MatCardModule,
